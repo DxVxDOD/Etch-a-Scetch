89610b8777959fbe1fd07a53a546efcda3581cd4
David Orbang <orbangdavid@yahoo.com>
2022-10-22 23:45:43 +0100

    grid layout fixed now it is scaleable to whatever size and stays in the said x y frame header done

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,7 @@ function makeCells (rows, cols) {
 
     for ( c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-        cell.innerText = (c + 1);
-        container.appendChild("cell").className = "grid-item";
+        container.appendChild(cell).className = "grid-item";
     }
 }
 
