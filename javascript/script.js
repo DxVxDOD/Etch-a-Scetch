fd46210e4a155a242b15ac39c41224d511eb4647
David Orbang <orbangdavid@yahoo.com>
2022-10-24 20:01:03 +0100

    color picker done buttons made grid paint function done

--- javascript/script.js
+++ javascript/script.js
@@ -14,54 +14,29 @@ function makeCells (rows, cols) {
 
 makeCells(16, 16);
 
-// Colour wheel logic
-
-/**
- * degreesToRadians
- * @param {number} degrees
- * @returns {number} radians
- */
-
-function degreesToRadians (degrees) {
-    return degrees * (Math.PI / 100);
+function clicked (e) {
+    let cleans = Array.from(document.querySelectorAll(".btn"));
+    cleans.forEach(clean =>clean.classList.remove("selected"));
+    e.target.classList.add("selected");
 }
 
-/**
- * generateColourWheel
- * @param {number} [size = 400]
- * @param {string} [centerColour = "white"]
- * @returns {HTMLCanvasElement}
- */
+const btns = Array.from(document.querySelectorAll(".btn"));
+btns.forEach(btn => btn.addEventListener("click", clicked))
 
-function generateColourWheel (size, centerColour) {
-    if (size === void 0) {size === 400; }
-    if (centerColour === void 0) {centerColour = "white"; }
-
-    // Generate main canvas to return
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
-    canvas.width = canvas.height = size;
-
-    // Generate canvasClone to draw increments on
-    var canvasClone = document.createElement("canvas");
-    canvasClone.width = canvasClone.height = size;
-    var canvasCloneCtx = canvasClone.getContext("2d");
-
-    //Initiate variables
-    var angle = 0;
-    var hexcode = 0;
-    var pivotPointer = 0;
-    var colourOffsetByDegree = 4.322;
-    
-    //For each degree in cricle perform operation
-    while (angle++ < 360) {
-        //find the index immediatelly befor and after pivot
-        var pivotPointerBefore = (pivotPointer + 3 - 1) % 3;
-        var pivotPointerAfter = (pivotPointer + 3 + 1) % 3;
-        //modify colours
-        if (hexcode[pivotPointer] < 255) {
-            // if main points is not full add main pointer
-            hexcode[pivotPointer] = (hexcode[pivotPointer] + colourOffsetByDegree > 255 ? 255 : hexcode[pivotPointer] + colourOffsetByDegree)
-        }
-    }
+function getColour (e) {
+    pickedColour = ""
+    pickedColour += e.target.value;
+    return pickedColour;
 }
+
+const colourPicker = document.getElementById("head");
+colourPicker.addEventListener("click", getColour)
+
+
+function paint (e) {
+    return e.target.style.backgroundColor = pickedColour;
+}
+
+const cells = Array.from(document.querySelectorAll(".grid-item"));
+cells.forEach(cell => cell.addEventListener("click", paint));
+cells.forEach(cell => cell.addEventListener("mousedown", "mousemove", paint));
